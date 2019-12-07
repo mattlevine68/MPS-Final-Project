1692e083e590c53fce05409fb39bbdf9a96767f1
mattlevine68 <51725771+mattlevine68@users.noreply.github.com>
2019-12-07 15:39:25 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include "init.h"
 
+
 int main(void){
 	Sys_Init();
-	//Testing Commit
+
 	// Add Handles for DCMI, DCMI DMA, I2C, Uart, Uart DMA, DMA2
 
 	//Intialize Everything
 
-	//Read GPIO Pin if variable = 1 take the picture and transmit 
+	//Read GPIO Pin if variable = 1 take the picture and transmit
 }
