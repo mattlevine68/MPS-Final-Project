770beeaadce2a4f6046f15ebd0a1cd0f38e0fb1b
mattlevine68 <51725771+mattlevine68@users.noreply.github.com>
2019-12-08 13:38:58 -0500

    started msp

--- src/dcmi.c
+++ src/dcmi.c
@@ -5,4 +5,7 @@
 // Peripheral's GPIO Configuration
 void HAL_DCMI_MspInit(DCMI_HandleTypeDef *hdcmi){
 	GPIO_InitTypeDef  GPIO_InitStruct;
+  if(hdcmi->Instance==DCMI){
+    __HAL_RCC_DCMI_CLK_ENABLE();
+  }
 }
