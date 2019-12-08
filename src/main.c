c64f1b48a5a2fef630444a18fc9d1fb2e812d67d
Matt Levine <51725771+mattlevine68@users.noreply.github.com>
2019-12-08 12:16:15 -0500

    DCMI Initialization

--- src/main.c
+++ src/main.c
@@ -4,12 +4,21 @@
 #include<stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-void GPIO_Setup(void);
-void ov5647_Snapshot(void); //Might not be void if needs to return value
+//Member Functions
+void GPIO_Setup();
+void ov5647_Snapshot(); //Might not be void if needs to return value
+void DCMI_Init();
+//Member Varibales
 GPIO_InitTypeDef GPIO_InitStruct;
+UART_HandleTypeDef huart3;
+DCMI_HandleTypeDef hdcmi;
+
+
 int main(void){
 	Sys_Init();
+	HAL_Init();
+	DCMI_Init();
+	initUart(&huart3, 115200,  USART3);
 
 	// Add Handles for DCMI, DCMI DMA, I2C, Uart, Uart DMA, DMA2
 
@@ -29,7 +38,7 @@ int main(void){
 
 
 
-GPIO_Setup(){
+void GPIO_Setup(){
 	//Input GPIO for values
 	__HAL_RCC_GPIOJ_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
@@ -46,6 +55,23 @@ GPIO_Setup(){
 
 }
 
-ov5647_Snapshot(){
+void ov5647_Snapshot(){
 	//Calls all the camera stuff
 }
+
+void DCMI_Init(){
+	hdcmi.Instance =DCMI;
+	hdcmi.Init.PCKPolarity = DCMI_PCKPOLARITY_RISING;
+	hdcmi.Init.VSPolarity = DCMI_VSPOLARITY_LOW;
+	hdcmi.Init.HSPolarity = DCMI_HSPOLARITY_LOW;
+	hdcmi.Init.CaptureRate = DCMI_CR_ALL_FRAME;
+	hdcmi.Init.ExtendedDataMode = DCMI_EXTEND_DATA_8B;
+	hdcmi.Init.JPEGMode = DCMI_JPEG_ENABLE;
+	hdcmi.Init.ByteSelectMode = DCMI_BSM_ALL;
+	hdcmi.Init.ByteSelectStart = DCMI_OEBS_ODD;
+	hdcmi.Init.LineSelectMode = DCMI_LSM_ALL;
+	hdcmi.Init.LineSelectStart = DCMI_OELS_ODD;
+
+	HAL_DCMI_Init(&hdcmi);
+}
+
